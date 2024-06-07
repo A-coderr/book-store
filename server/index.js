9b1ef545ca0f562271210bfff332fd25b2d9fa7f
Anzhelika Kostyuk <akostyuk@conestogac.on.ca>
2024-06-07 15:59:25 -0400

    Added a method to delete a book by id

--- server/index.js
+++ server/index.js
@@ -80,6 +80,21 @@ app.put('/books/:id', async (request, response) => {
     }
 });
 
+//Route to delete a book
+app.delete('/books/:id', async (request, response) => {
+    try {
+        const {id} = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        if(!result){
+            return response.status(404).send({message: "Book not found"});
+        }; 
+        return response.status(200).send({message: "Book deleted successfully"});           
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
 mongoose
 .connect(mongoDBURL)
 .then(() => {
