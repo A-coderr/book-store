7cb22f07b980ce36a3ba7c92d0f288e146e7fa10
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2024-06-07 22:27:49 -0400

    Added routes
    Created new pages
    Added react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import AddBook from './pages/AddBook'
+import DeleteBook from './pages/DeleteBook'
+import EditBook from './pages/EditBook'
+import ShowBooks from './pages/ShowBooks'
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>
-      Hello World
-    </div>
+  <Routes>
+    <Route path='/' element={<Home></Home>}></Route>
+    <Route path='/books/create' element={<AddBook></AddBook>}></Route>
+    <Route path='/books/details/:id' element={<ShowBooks></ShowBooks>}></Route>
+    <Route path='/books/edit/:id' element={<EditBook></EditBook>}></Route>
+    <Route path='/books/delete/:id' element={<DeleteBook></DeleteBook>}></Route>
+  </Routes>
   )
 }
 
