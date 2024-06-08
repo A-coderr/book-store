e7d2f05cdb54cf807306ba73ebad9e3169a06bce
Anzhelika Kostyuk <kostyukanzhelika@gmail.com>
2024-06-07 22:55:01 -0400

    Added table

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ const app = express();
 app.use(express.json());
 
 //Middleware for handling CORS Policy
-app.use(
+/* app.use(
     cors({
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type'],
     })
-);
+); */
 
 app.get('/', (request, response) => {
     console.log(request);
